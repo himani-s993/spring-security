87e6e5e917041aaefbd71657d49077e05ca5de6a
Himani Sharma <himani.sharma@weboniselab.com>
2017-12-19 19:17:39 +0530

    Create basic structure

---
+++ src/main/java/com/webonise/spring/security/springsecurity/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.webonise.spring.security.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/unauth")
+    public String unauth() {
+        return "home";
+    }
+}
---
+++ src/main/java/com/webonise/spring/security/springsecurity/service/impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.webonise.spring.security.springsecurity.service.impl;
+
+import com.webonise.spring.security.springsecurity.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+
+}
